0669fa5b9b5f9de8fd10212e170a0502eae384c3
Oleksandr Pylypchuk <oleksandr.pylypchuk@intenselab.eu>
2026-02-22 12:50:58 +0200

    Trip model and duplicate tracker

---
+++ CrewRed-Test/Models/Trip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrewRed_Test.Models
+{
+    public class Trip
+    {
+        public DateTime Pickup { get; set; }
+        public DateTime Dropoff { get; set; }
+        public byte PassengerCount { get; set; }
+        public decimal TripDistance { get; set; }
+        public string StoreAndFwdFlag { get; set; }
+        public int PULocationID { get; set; }
+        public int DOLocationID { get; set; }
+        public decimal FareAmount { get; set; }
+        public decimal TipAmount { get; set; }
+    }
+}
---
+++ CrewRed-Test/Services/DuplicateTracker.cs
@@ -0,0 +1,34 @@
+﻿using CrewRed_Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrewRed_Test.Services
+{
+    public class DuplicateTracker
+    {
+        private readonly HashSet<string> _keys = new();
+        private readonly StreamWriter _duplicateWriter;
+
+        public DuplicateTracker(string duplicateFilePath)
+        {
+            _duplicateWriter = new StreamWriter(duplicateFilePath);
+        }
+
+        public bool IsDuplicate(Trip trip)
+        {
+            var key = $"{trip.Pickup:o}|{trip.Dropoff:o}|{trip.PassengerCount}";
+
+            if (_keys.Contains(key))
+            {
+                _duplicateWriter.WriteLine(key);
+                return true;
+            }
+
+            _keys.Add(key);
+            return false;
+        }
+    }
+}
